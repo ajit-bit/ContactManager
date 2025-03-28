abe60d5a6dba0f98aab08af2db30c0e5103a457c
ajit-bit <ajit1234pillai@gmail.com>
2025-03-28 21:23:17 +0530

    Fix line endings issue

---
+++ controllers/contactControllers.js
@@ -0,0 +1,33 @@
+const getContacts = (req, res) => {  
+    res.status(200).json({ message: "Get all contacts" });
+};
+
+const createContact = (req, res) => {  
+    console.log("the request body is :",req.body);
+    const {name,email,phone}=req.body;
+    if(!name||!email||!phone){
+        res.status(400);
+        throw new error("All feilds are mandatory");
+    }
+    res.status(201).json({ message: "Create contact" });
+};
+
+const getContact = (req, res) => {  
+    res.status(200).json({ message: `Get contact for ${req.params.id}` });
+};
+
+const updateContact = (req, res) => {  
+    res.status(200).json({ message: `Update contact for ${req.params.id}` });
+};
+
+const deleteContact = (req, res) => {  
+    res.status(200).json({ message: `Delete contact for ${req.params.id}` });
+};
+
+module.exports = {
+    getContacts,   // Fixed naming
+    createContact,
+    getContact,
+    updateContact,  // Fixed naming
+    deleteContact
+};
